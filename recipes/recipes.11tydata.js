96f86495a0b2562f876f66d0b4a7f710ad6b4ad8
Carlos Flores <carlos@floresbenavides.com>
2026-08-14 23:30:10 -0600

    move recipe layout and tags to directory data file

---
+++ recipes/recipes.11tydata.js
@@ -0,0 +1,6 @@
+// Data file de directorio: aplica a todas las recetas (recipes/*.html).
+// layout y tags son idénticos en todas, así que viven aquí en vez del front matter.
+export default {
+  layout: "receta.njk",
+  tags: "receta",
+};
